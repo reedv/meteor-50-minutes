54868a147940eb993f6022626c3386c08a008506
reedv <villanuevareed@gmail.com>
2016-10-09 14:36:45 -1000

    Task 5: subscriptions

--- app/tasklist.js
+++ app/tasklist.js
@@ -5,6 +5,9 @@
 Tasks = new Mongo.Collection('tasks')
 
 if (Meteor.isClient) {
+  // Name of a subscription. Matches the name of the server's `publish()` call.
+  Meteor.subscribe('tasks');
+
   Template.tasks.helpers({
     tasks: function () {
       return Tasks.find({}, { sort: { createdAt: -1 } })
@@ -38,20 +41,26 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  Meteor.publish('tasks', function(){
+    // Function called on the server each time a client subscribes
+    //   to the record set 'tasks'
 
+    // return all Tasks items associated with the current user's userID
+    return Tasks.find({userID: this.userId});
+  })
 }
 
 // using methods is more secure and allows us to keep all functions in one place
 Meteor.methods({
   addTask: function(name) {
     // check if user is logged in
-    if(!Meteor.userID()) {
+    if(!Meteor.userId()) {
       throw new Meteor.Error('No Access');
     }
 
     // add an obj. with 'name' and 'createdAt' date to the DB
     Tasks.insert({
-      name: name, createdAt: new Date(), userID: Meteor.userID()
+      name: name, createdAt: new Date(), userID: Meteor.userId()
     });
   },
 
